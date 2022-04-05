73eb6ff36d4396f9afa222f1ee287f22617fe151
Ranojoy Chatterjee <Rano@Rano-iMac.local>
2022-04-05 19:29:13 -0400

    minor changes

--- data_curation.R
+++ data_curation.R
@@ -22,32 +22,6 @@ rownames(wound1) <- wound1[,1]
 wound1[,1] <- NULL
 rownames(wound1) <- paste(row.names(wound1), c("_s1"))
 
-
-wound2 <- read.table(file = 'analysis/data/data_labelled_Wound2.tsv', sep = '\t', header = TRUE)
-rownames(wound2) <- wound2[,1]
-wound2[,1] <- NULL
-rownames(wound2) <- paste(row.names(wound2), c("_s2"))
-
-nonwound1 <- read.table(file = 'analysis/data/data_labelled_Nonwound1.tsv', sep = '\t', header = TRUE)
-rownames(nonwound1) <- nonwound1[,1]
-nonwound1[,1] <- NULL
-rownames(nonwound1) <- paste(row.names(nonwound1), c("_s3"))
-
-
-nonwound2 <- read.table(file = 'analysis/data/data_labelled_Nonwound2.tsv', sep = '\t', header = TRUE)
-rownames(nonwound2) <- nonwound2[,1]
-nonwound2[,1] <- NULL
-rownames(nonwound2) <- paste(row.names(nonwound2), c("_s4"))
-
-#compare_df_rows_same(test, test_un)
-#final <- rbind(test,test_un)
-##Merging the 4 seperate samples as a master dataset
-test <- dplyr::bind_rows(wound1, wound2)
-test <- dplyr::bind_rows(test, nonwound1)
-test <- dplyr::bind_rows(test, nonwound2)
-
-##Import the metadata from the BOX folder
-
 meta_wound1 <- read.table(file = 'analysis/data/Label_Wound1.tsv', sep = '\t', header = TRUE)
 rownames(meta_wound1) <- meta_wound1[,1]
 meta_wound1[,1] <- NULL
@@ -58,6 +32,18 @@ colmn <- paste("col", 1:3)
 meta_wound1<- meta_wound1 %>% separate(ref.data.fine, sep = " ", into = colmn, remove = FALSE)
 meta_wound1 <- meta_wound1[ , -which(names(meta_wound1) %in% c("col 2","col 3"))]
 names(meta_wound1)[names(meta_wound1) == 'col 1'] <- "Cell_type"
+
+###Merging the meta data and the data####
+df_merge <- merge(wound1,meta_wound1,by = 0, all = T)
+df_merge <- t(df_merge)
+####Saving the combined dataset####
+write.table(df_merge,"transpose/Wound1_full_transpose.tsv",  sep = "\t", eol = "\n", quote = F, col.names = NA, row.names = T)
+
+
+wound2 <- read.table(file = 'analysis/data/data_labelled_Wound2.tsv', sep = '\t', header = TRUE)
+rownames(wound2) <- wound2[,1]
+wound2[,1] <- NULL
+rownames(wound2) <- paste(row.names(wound2), c("_s2"))
 
 meta_wound2 <- read.table(file = 'analysis/data/Label_Wound2.tsv', sep = '\t', header = TRUE)
 rownames(meta_wound2) <- meta_wound2[,1]
@@ -70,6 +56,17 @@ meta_wound2<- meta_wound2 %>% separate(ref.data.fine, sep = " ", into = colmn, r
 meta_wound2 <- meta_wound2[ , -which(names(meta_wound2) %in% c("col 2","col 3"))]
 names(meta_wound2)[names(meta_wound2) == 'col 1'] <- "Cell_type"
 
+###Merging the meta data and the data####
+df_merge <- merge(wound2,meta_wound2,by = 0, all = T)
+df_merge <- t(df_merge)
+####Saving the combined dataset####
+write.table(df_merge,"transpose/Wound2_full_transpose.tsv",  sep = "\t", eol = "\n", quote = F, col.names = NA, row.names = T)
+
+nonwound1 <- read.table(file = 'analysis/data/data_labelled_Nonwound1.tsv', sep = '\t', header = TRUE)
+rownames(nonwound1) <- nonwound1[,1]
+nonwound1[,1] <- NULL
+rownames(nonwound1) <- paste(row.names(nonwound1), c("_s3"))
+
 meta_nonwound1 <- read.table(file = 'analysis/data/Label_Nonwound1.tsv', sep = '\t', header = TRUE)
 rownames(meta_nonwound1) <- meta_nonwound1[,1]
 meta_nonwound1[,1] <- NULL
@@ -80,7 +77,17 @@ colmn <- paste("col", 1:3)
 meta_nonwound1<- meta_nonwound1 %>% separate(ref.data.fine, sep = " ", into = colmn, remove = FALSE)
 meta_nonwound1 <- meta_nonwound1[ , -which(names(meta_nonwound1) %in% c("col 2","col 3"))]
 names(meta_nonwound1)[names(meta_nonwound1) == 'col 1'] <- "Cell_type"
+###Merging the meta data and the data####
+df_merge <- merge(nonwound1,meta_nonwound1,by = 0, all = T)
+df_merge <- t(df_merge)
+####Saving the combined dataset####
+write.table(df_merge,"transpose/Nonwound1_full_transpose.tsv",  sep = "\t", eol = "\n", quote = F, col.names = NA, row.names = T)
 
+
+nonwound2 <- read.table(file = 'analysis/data/data_labelled_Nonwound2.tsv', sep = '\t', header = TRUE)
+rownames(nonwound2) <- nonwound2[,1]
+nonwound2[,1] <- NULL
+rownames(nonwound2) <- paste(row.names(nonwound2), c("_s4"))
 
 meta_nonwound2 <- read.table(file = 'analysis/data/Label_Nonwound2.tsv', sep = '\t', header = TRUE)
 rownames(meta_nonwound2) <- meta_nonwound2[,1]
@@ -92,6 +99,19 @@ colmn <- paste("col", 1:3)
 meta_nonwound2<- meta_nonwound2 %>% separate(ref.data.fine, sep = " ", into = colmn, remove = FALSE)
 meta_nonwound2 <- meta_nonwound2[ , -which(names(meta_nonwound2) %in% c("col 2","col 3"))]
 names(meta_nonwound2)[names(meta_nonwound2) == 'col 1'] <- "Cell_type"
+
+###Merging the meta data and the data####
+df_merge <- merge(nonwound2,meta_nonwound2,by = 0, all = T)
+df_merge <- t(df_merge)
+####Saving the combined dataset####
+write.table(df_merge,"transpose/Nonwound2_full_transpose.tsv",  sep = "\t", eol = "\n", quote = F, col.names = NA, row.names = T)
+#compare_df_rows_same(test, test_un)
+#final <- rbind(test,test_un)
+##Merging the 4 seperate samples as a master dataset
+test <- dplyr::bind_rows(wound1, wound2)
+test <- dplyr::bind_rows(test, nonwound1)
+test <- dplyr::bind_rows(test, nonwound2)
+
 
 ##Merging the 4 seperate samples as a master meta dataset
 meta_test <- dplyr::bind_rows(meta_wound1, meta_wound2)
