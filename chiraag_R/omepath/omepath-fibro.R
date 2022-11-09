71edf2c95436aa8f674e78cf1faf2fc9ff83e058
Chiraag <cgohel@gmail.com>
2022-11-08 20:29:35 -0500

    added omepath-fibro.R

---
+++ chiraag_R/omepath/omepath-fibro.R
@@ -0,0 +1,54 @@
+# libraries ---------------------------------------------------------------
+
+library(devtools)
+library(omePath)
+library(clusterProfiler)
+library(enrichplot)
+library(ggplot2)
+library(limma)
+library(KEGGREST)
+library(tidyverse)
+library(msigdbr)
+library(fgsea)
+library(org.Mm.eg.db)
+
+data_dir <-
+  "~/box/snRNA_CellRanger_Wound_nonWound/analysis/Tweedieverse_output_Fibroblasts/"
+df <- read.table(
+  file = paste0(data_dir, "all_results.tsv"),
+  sep = '\t',
+  header = TRUE
+)
+
+ids <-
+  bitr(
+    df$feature,
+    fromType = "SYMBOL",
+    toType = "ENTREZID",
+    OrgDb = org.Mm.eg.db
+  )
+
+tab <- getGeneKEGGLinks(species.KEGG = "mmu")
+tab$Symbol <- mapIds(org.Mm.eg.db,
+                     tab$GeneID,
+                     column = "SYMBOL",
+                     keytype = "ENTREZID")
+
+gene_intersect <- intersect(tab$Symbol, df$feature)
+df <- df %>%
+  filter(feature %in% gene_intersect)
+
+tab <- tab %>%
+  filter(Symbol %in% gene_intersect)
+
+omepath_results <- omePath(
+  as.data.frame(df, row.names = df$feature),
+  "output_deepath",
+  as.data.frame(tab, row.names = tab$Symbol),
+  pathway_col = "PathwayID",
+  feature_col = "Symbol",
+  score_col = 'coef',
+  method = 'wilcox',
+  min_member = 2,
+  do_plot = TRUE
+)
