6047859ff65c51e76f4055275c12365b12d24b09
Ranojoy <50858727+ranojoychatterjee@users.noreply.github.com>
2024-01-09 01:50:52 -0500

    Minor changes

--- R/sctansform_integration.R
+++ R/sctansform_integration.R
@@ -52,8 +52,8 @@ sctransform_integration<-function(obj_list,output_directory){
       cat("Directory already exists:", directory_path, "\n")
     }
     file <- paste0(output_directory,"sctransform.rds")
-    saveRDS(obj_list,directory_path)
-    return(obj_list)
-  }
+    saveRDS(obj_list,file)
 
+  }
+  return(obj_list)
 }
