145478fb2fd2b1e70f490bc296d8fdb675398515
conference <conference@SPH-1Y75NC2.ead.gwu.edu>
2024-10-17 15:23:20 -0400

    Minor change

--- R/cellcomm.R
+++ R/cellcomm.R
@@ -54,6 +54,49 @@ cellcomm_analysis<-function(obj_list,output_directory,imp_var,species){
     cellchat_normal <- aggregateNet(cellchat_normal,thresh = 0.05)
     cellchat_normal@net$pathways <- df_significant_netP$Var3
     groupSize <- as.numeric(table(cellchat_normal@idents))
+
+    # Define the directory and check if it exists
+    fig_dir <- paste0(output_directory,"/cellcommunication/")
+
+    # If the directory doesn't exist, create it
+    if (!dir.exists(fig_dir)) {
+      dir.create(fig_dir, recursive = TRUE)
+    }
+
+    # Open the SVG device for the first plot
+    svglite(file = paste0(fig_dir, "pathway_count.svg"), width = 11, height = 10)
+
+    # Set up for two plots side by side
+    par(mfrow = c(1, 2), xpd = TRUE, cex.main = 1.5, font.main = 2)
+
+    # Plot the number of interactions
+    netVisual_circle(cellchat_normal@net$count,
+                     vertex.weight = groupSize,
+                     weight.scale = TRUE,
+                     label.edge = FALSE,
+                     title.name = "Number of interactions",
+                     vertex.label.cex = 0.5)
+
+    # Close the device after saving the first plot
+    dev.off()
+
+    # Open the SVG device for the second plot
+    svglite(file = paste0(fig_dir, "pathway_weight.svg"), width = 11, height = 7.2)
+
+    # Set up for two plots side by side again
+    par(mfrow = c(1, 2), xpd = TRUE, cex.main = 1.5, font.main = 2)
+
+    # Plot the interaction weights/strength
+    netVisual_circle(cellchat_normal@net$weight,
+                     vertex.weight = groupSize,
+                     weight.scale = TRUE,
+                     label.edge = FALSE,
+                     title.name = "Interaction weights/strength",
+                     vertex.label.cex = 0.5)
+
+    # Close the device after saving the second plot
+    dev.off()
+
   }
   else{
     obj_list <- pca_clusters
@@ -102,5 +145,46 @@ cellcomm_analysis<-function(obj_list,output_directory,imp_var,species){
     cellchat_normal <- aggregateNet(cellchat_normal,thresh = 0.05)
     cellchat_normal@net$pathways <- df_significant_netP$Var3
     groupSize <- as.numeric(table(cellchat_normal@idents))
+    # Define the directory and check if it exists
+    fig_dir <- paste0(output_directory,"/cellcommunication/")
+
+    # If the directory doesn't exist, create it
+    if (!dir.exists(fig_dir)) {
+      dir.create(fig_dir, recursive = TRUE)
+    }
+
+    # Open the SVG device for the first plot
+    svglite(file = paste0(fig_dir, "pathway_count.svg"), width = 11, height = 10)
+
+    # Set up for two plots side by side
+    par(mfrow = c(1, 2), xpd = TRUE, cex.main = 1.5, font.main = 2)
+
+    # Plot the number of interactions
+    netVisual_circle(cellchat_normal@net$count,
+                     vertex.weight = groupSize,
+                     weight.scale = TRUE,
+                     label.edge = FALSE,
+                     title.name = "Number of interactions",
+                     vertex.label.cex = 0.5)
+
+    # Close the device after saving the first plot
+    dev.off()
+
+    # Open the SVG device for the second plot
+    svglite(file = paste0(fig_dir, "pathway_weight.svg"), width = 11, height = 7.2)
+
+    # Set up for two plots side by side again
+    par(mfrow = c(1, 2), xpd = TRUE, cex.main = 1.5, font.main = 2)
+
+    # Plot the interaction weights/strength
+    netVisual_circle(cellchat_normal@net$weight,
+                     vertex.weight = groupSize,
+                     weight.scale = TRUE,
+                     label.edge = FALSE,
+                     title.name = "Interaction weights/strength",
+                     vertex.label.cex = 0.5)
+
+    # Close the device after saving the second plot
+    dev.off()
   }
 }
